69e79ef396212b2e1e2246a3b1bb74a3609d6220
Caner <ozdemircnr@gmail.com>
2023-09-19 17:36:36 +0300

    test

---
+++ NewRepoCaner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewRepoCaner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
